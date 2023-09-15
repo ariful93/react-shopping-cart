d567842072a9c0b13a0ae7b93a472a57d3779ad5
Ariful Islam <arif13414@gmail.com>
2023-09-15 08:23:23 +0600

    Remove unnessacery code

--- src/App.jsx
+++ src/App.jsx
@@ -8,64 +8,7 @@ import Cart from './pages/Cart';
 import Shop from './pages/Shop';
 
 function App() {
-
-  // const [state, setState] = useState({
-  //   PRODUCTS: PRODUCTS,
-  //   cart: [],
-  // });
-
-  // const addToCart = (product) => {
-  //   setState({
-  //     ...state,
-  //     cart: state.cart.find((cartItem) => cartItem.id === product.id)
-  //       ? state.cart.map((cartItem) =>
-  //           cartItem.id === product.id
-  //             ? { ...cartItem, count: cartItem.count + 1 }
-  //             : cartItem
-  //         )
-  //       : [...state.cart, { ...product, count: 1 }],
-  //   });
-  // };
-
-  // const increase = (product) => {
-  //   setState({
-  //     ...state,
-  //     cart: state.cart.map((cartItem) =>
-  //       cartItem.id === product.id
-  //         ? { ...cartItem, count: cartItem.count + 1 }
-  //         : cartItem
-  //     ),
-  //   });
-  // };
-
-  // const decrease = (product) => {
-  //   setState({
-  //     ...state,
-  //     cart: state.cart.map((cartItem) =>
-  //       cartItem.id === product.id
-  //         ? { ...cartItem, count: cartItem.count > 1 ? cartItem.count - 1 : 1 }
-  //         : cartItem
-  //     ),
-  //   });
-  // };
-
-  // const removeItem = (id) => {
-  //   setState({
-  //     ...state,
-  //     cart: state.cart.filter((cartItem) => cartItem.id !== id),
-  //   });
-  // };
-
-  // const cartItemCount = state.cart.reduce(
-  //   (acc, data) => (acc += data.count),
-  //   0
-  // );
-
-  // const cartTotals = state.cart.reduce(
-  //   (acc, data) => (acc += data.price * data.count),
-  //   0
-  // );
-
+  
   return (
     <div className='shopping-cart'>
       <CartContextProvider>
